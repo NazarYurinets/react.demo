f872bba6fb71a1ffd1bcd9d44e2d09ab42be5b71
nazar <nazar.yurynets@gmail.com>
2022-09-15 18:28:51 +0300

    react1

---
+++ src/component/Simpson.js
@@ -0,0 +1,12 @@
+
+function Simpson(props){
+    const {simpson} = props
+    return(
+        <div>
+        <h2>{simpson.name} -- {simpson.age}</h2>
+            <p>{simpson.surname} -- {simpson.info}</p>
+            <img src={simpson.photo} alt={simpson.name}/>
+        </div>
+    );
+}
+export default Simpson;
